1063bf70b456f52211f0bfaae0e890f75517fedc
Patrick Ambrose <patrickambrosericky@gmail.com>
2025-01-01 20:14:56 +0530

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,12 @@
-# PowerShell $PROFILE Customization
+# Pat's PowerShell Profile Customization
 
-# Disabling Directory Hightlight (PowerShell 7.3 specific)
-$PSStyle.FileInfo.Directory = "" # Comment if you are using PowerShell 7.2 or lower
+# Disable Directory Highlights (only for PowerShell versions > 7.3)
+$PSStyle.FileInfo.Directory = "" 
 
-# Shell Autocompletion
-# Original Source: https://dev.to/dhravya/how-to-add-autocomplete-to-powershell-in-30-seconds-2a8p
-
-# Importing Terminal Icons
+# Setup Terminal Icons
 Import-Module Terminal-Icons
 
-# Initializing the module
+# Setup PSReadLine for commandline text completion
 Import-Module PSReadLine
 
 # Shows navigable menu of all options when hitting Tab
@@ -32,105 +29,11 @@ $ENV:STARSHIP_DISTRO = " "
 Invoke-Expression (&starship init powershell)
 
 # Display FastFetch Output on Screen
-Write-Output "`n"
 & fastfetch.exe
-
-# Custom Functions
-# Editor Configuration
-# Utility Functions
-function Test-CommandExists {
-    param($command)
-    $exists = $null -ne (Get-Command $command -ErrorAction SilentlyContinue)
-    return $exists
-}
-
-$EDITOR = if (Test-CommandExists code) { 'code' }
-elseif (Test-CommandExists nvim) { 'nvim' }
-elseif (Test-CommandExists pvim) { 'pvim' }
-elseif (Test-CommandExists vim) { 'vim' }
-elseif (Test-CommandExists vi) { 'vi' }
-else { 'notepad' }
-Set-Alias -Name edit -Value $EDITOR
-
-function lsf {
-    param(
-        [switch]$Recursive
-    )
-
-    if ($Recursive) {
-        Get-ChildItem -File -Recurse | Format-Table -Property Mode, LastWriteTime, Length, FullName -AutoSize
-    }
-    else {
-        Get-ChildItem -File | Format-Table -Property Mode, LastWriteTime, Length, FullName -AutoSize
-    }
-}
-
-function lsd {
-    param(
-        [string]$Path = (Get-Location),
-        [switch]$Recursive
-    )
-
-    # Resolve the full path
-    $fullPath = Resolve-Path -Path $Path
-
-    # Check if the specified path exists and is a directory
-    if (!(Test-Path -Path $fullPath -PathType Container)) {
-        Write-Host "The specified path '$Path' does not exist or is not a directory." -ForegroundColor Red
-        return
-    }
-
-    # Get the directories (recursive or not)
-    $dirs = if ($Recursive) {
-        Get-ChildItem -Directory -Path $fullPath -Recurse
-    } else {
-        Get-ChildItem -Directory -Path $fullPath
-    }
-
-    # Check if any directories are found
-    if (-not $dirs) {
-        Write-Host "No directories found in '$fullPath'." -ForegroundColor Yellow
-        return
-    }
-
-    # Truncate directory paths to be relative to the specified path
-    $relativeDirs = $dirs | ForEach-Object {
-        $_ | Add-Member -MemberType NoteProperty -Name RelativePath -Value ($_.FullName -replace [regex]::Escape($fullPath), "")
-        $_
-    }
-
-    # Format the output table with truncated directory paths
-    $relativeDirs | Format-Table -Property Mode, LastWriteTime, @{Name = "Path"; Expression = { $_.RelativePath } } -AutoSize
-
-    # Print summary
-    Write-Host "Directories listed from: $fullPath" -ForegroundColor Green
-    Write-Host "Found $($relativeDirs.Count) Directories" -ForegroundColor Cyan
-}
-
-
-# Usage examples:
-# lsd                        # Lists directories in the current directory
-# lsd -Path "C:\SomeFolder"  # Lists directories in the specified directory
-# lsd -r                     # Lists directories in the current directory 
-
 
 # Make a Directory and cd into it
 function mkcd { param($dir) mkdir $dir -Force; Set-Location $dir }
 
-# System Utilities
-function admin {
-    if ($args.Count -gt 0) {
-        # Combine the arguments into a single string
-        $argList = $args -join ' '
-        # Run the command as administrator in Windows Terminal
-        Start-Process wt -Verb runAs -ArgumentList "pwsh.exe -NoExit -Command $argList"
-    } else {
-        Start-Process wt -Verb runAs
-    }
-}
-
-# Set alias
-Set-Alias -Name su -Value admin
 
 function touch($file) { "" | Out-File $file -Encoding ASCII }
 function ff($name) {
@@ -139,29 +42,26 @@ function ff($name) {
     }
 }
 
-function uptime {
-    if ($PSVersionTable.PSVersion.Major -eq 5) {
-        # Get system start time using WMI for PowerShell 5
-        $lastBootUpTime = Get-WmiObject win32_operatingsystem | 
-            Select-Object @{Name='LastBootUpTime'; Expression={ $_.ConverttoDateTime($_.lastbootuptime) }} |
-            Select-Object -ExpandProperty LastBootUpTime
-    } else {
-        # Get system start time using net statistics for PowerShell 7+
-        $lastBootUpTimeString = net statistics workstation | Select-String "since" | ForEach-Object { $_.ToString().Replace('Statistics since ', '') }
-        $lastBootUpTime = [datetime]::Parse($lastBootUpTimeString)
+function locate {
+    param (
+        [string]$CMD,
+        [switch]$Yank
+    )
+
+    $path = Get-Command -Name $CMD -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+
+    if ($path) {
+        if ($Yank.IsPresent) {
+            $path | Set-Clipboard
+            Write-Host "Path copied to clipboard:" $path -ForegroundColor Green
+        }
+        else {
+            Write-Host $path
+        }
     }
-
-    # Calculate elapsed time since system start
-    $uptimeSpan = New-TimeSpan -Start $lastBootUpTime
-
-    # Create a custom object with system start time and uptime in HH:MM:SS
-    $uptimeInfo = [pscustomobject]@{
-        'Login Time'       = $lastBootUpTime
-        'Elapsed Time'     = "{0:D2}:{1:D2}:{2:D2}" -f $uptimeSpan.Hours, $uptimeSpan.Minutes, $uptimeSpan.Seconds
+    else {
+        Write-Host "Unable to find $CMD" -ForegroundColor Red
     }
-
-    # Output the object
-    return $uptimeInfo
 }
 
 
@@ -174,15 +74,15 @@ function gc { param($m) git commit -m "$m" }
 
 function gp { git push }
 
-# Navigation Shortcuts
-function docs { Set-Location -Path $HOME\Documents }
-
-function dtop { Set-Location -Path $HOME\Desktop }
-
-function down { Set-Location -Path $HOME\Downloads }
-
-function repo { Set-Location -Path $HOME\Documents\Git-Repositories } 
-
 # Activate Zoxide
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
 
+# Setup configurations for bat
+$ENV:BAT_CONFIG_PATH = "$ENV:USERPROFILE/.config/bat/bat.conf"
+$ENV:BAT_CONFIG_DIR = "$ENV:USERPROFILE/.config/bat"
+Set-Alias -Name cat -Value bat
+
+
+function Import-PSProfile {
+    . $PROFILE
+}
